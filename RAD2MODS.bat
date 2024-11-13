431792079641b34ec251847e1104b3de4b503c89
Dzxak - Cloud <dzxakoffc@gmail.com>
2024-11-13 17:20:50 +0700

    Update RAD2MODS.bat

--- RAD2MODS.bat
+++ RAD2MODS.bat
@@ -5,7 +5,7 @@ color 0A
 set "mod_folder=%APPDATA%\.minecraft"
 
 :: Link to the mods.zip file
-set "mods_zip_url="https://file.io/QPkp3CT9djvS"
+set "mods_zip_url="http://206.189.144.244:8080/api/raw/mods.zip?auth=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJsb2NhbGUiOiJlbiIsInZpZXdNb2RlIjoibGlzdCIsInNpbmdsZUNsaWNrIjpmYWxzZSwicGVybSI6eyJhZG1pbiI6dHJ1ZSwiZXhlY3V0ZSI6dHJ1ZSwiY3JlYXRlIjp0cnVlLCJyZW5hbWUiOnRydWUsIm1vZGlmeSI6dHJ1ZSwiZGVsZXRlIjp0cnVlLCJzaGFyZSI6dHJ1ZSwiZG93bmxvYWQiOnRydWV9LCJjb21tYW5kcyI6WyJzdWRvIiwiYmFzaCIsInNoIiwibHMiLCJjYXQiLCJlY2hvIiwidGVlIiwicHdkIiwiZGYiLCJ3aG9hbWkiLCJpZCIsIm1rZGlyIl0sImxvY2tQYXNzd29yZCI6ZmFsc2UsImhpZGVEb3RmaWxlcyI6ZmFsc2UsImRhdGVGb3JtYXQiOmZhbHNlfSwiaXNzIjoiRmlsZSBCcm93c2VyIiwiZXhwIjoxNzMxNTAwMTA2LCJpYXQiOjE3MzE0OTI5MDZ9.h4L6fB8OeH-EbMze5Cdw-5ln1v8z25GDmdPub8b3C58&"
 set "zip_path=%APPDATA%\.minecraft\mods.zip"
 
 cls
